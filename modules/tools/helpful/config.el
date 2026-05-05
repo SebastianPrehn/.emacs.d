b56e5e04f5da081e1949900e89fc53cb0e93be9a
Sebastian Larsen Prehn <sbstprehn@icloud.com>
2026-05-05 15:29:05 +0200

    add helpful

---
+++ modules/tools/helpful/config.el
@@ -0,0 +1,13 @@
+;;; helpful/config.el --- Helpful -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(use-package helpful
+  :demand t
+  :config
+  (require 'shortdoc)) ;; silences native-comp warning
+
+
+
+(provide 'slp-helpful)
+;;; config.el ends here
